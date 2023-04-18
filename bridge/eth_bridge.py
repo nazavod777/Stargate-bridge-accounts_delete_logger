6a9591791d70156af22c88d29fd30ac769e256b1
nazavod777 <91217742+nazavod777@users.noreply.github.com>
2023-04-18 23:18:50 +0300

    Update eth_bridge.py

--- bridge/eth_bridge.py
+++ bridge/eth_bridge.py
@@ -1,7 +1,7 @@
 import json
 import time
 from web3 import Web3
-from web3_checksumm.get_checksum_address import get_checksum_address
+from web3.auto import w3
 
 # enter slippage as shown => 1 = 0.1%, 5 = 0.5%, 10 = 1%
 SLIPPAGE = 5
@@ -14,12 +14,12 @@ arbitrum_w3 = Web3(Web3.HTTPProvider(arbitrum_rpc_url))
 optimism_w3 = Web3(Web3.HTTPProvider(optimism_rpc_url))
 
 # Stargate Router
-stargate_arbitrum_address = get_checksum_address('0x53Bf833A5d6c4ddA888F69c22C88C9f356a41614')
-stargate_optimism_address = get_checksum_address('0xB0D502E938ed5f4df2E681fE6E419ff29631d62b')
+stargate_arbitrum_address = w3.to_checksum_address('0x53Bf833A5d6c4ddA888F69c22C88C9f356a41614')
+stargate_optimism_address = w3.to_checksum_address('0xB0D502E938ed5f4df2E681fE6E419ff29631d62b')
 
 # Stargate ETH Router
-stargate_arbitrum_eth_address = get_checksum_address('0xbf22f0f184bCcbeA268dF387a49fF5238dD23E40')
-stargate_optimism_eth_address = get_checksum_address('0xB49c4e680174E331CB0A7fF3Ab58afC9738d5F8b')
+stargate_arbitrum_eth_address = w3.to_checksum_address('0xbf22f0f184bCcbeA268dF387a49fF5238dD23E40')
+stargate_optimism_eth_address = w3.to_checksum_address('0xB49c4e680174E331CB0A7fF3Ab58afC9738d5F8b')
 
 # ABIs
 router_abi = json.load(open('./abis/router_abi.json'))
@@ -44,7 +44,7 @@ def get_balance_eth_optimism(address):
 
 
 def swap_eth_arbitrum_optimism(account, amount):
-    address = get_checksum_address(account=account)
+    address = w3.to_checksum_address(account.address)
     nonce = arbitrum_w3.eth.get_transaction_count(address)
     gas_price = arbitrum_w3.eth.gas_price
     fees = stargate_arbitrum_router_contract.functions.quoteLayerZeroFee(111,
@@ -73,7 +73,7 @@ def swap_eth_arbitrum_optimism(account, amount):
 
 
 def swap_eth_optimism_arbitrum(account, amount):
-    address = get_checksum_address(account=account)
+    address = w3.to_checksum_address(account.address)
     nonce = optimism_w3.eth.get_transaction_count(address)
     gas_price = optimism_w3.eth.gas_price
     fees = stargate_optimism_router_contract.functions.quoteLayerZeroFee(110,
